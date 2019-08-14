8fae931bf63ebaf3791fa5957f5a5daa7297a287
Ruben Ruiz <rubenruizmarron@gmail.com>
2019-08-14 11:47:46 -0500

    Added research card models and Api file

---
+++ ResearchToolsBackend/Models/ResearchCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResearchToolsBackend.Models
+{
+    public class ResearchCard
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public string ProjectId { get; set; }
+        public string Source { get; set; }
+        public string Quote { get; set; }
+        public string Summary { get; set; }
+        public string Commentary { get; set; }
+        public List<string> Keywords { get; set; }
+    }
+}
---
+++ ResearchToolsBackend/Models/ResearchCardUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResearchToolsBackend.Models
+{
+    class ResearchCardUpdateModel
+    {
+        public string Source { get; set; }
+        public string Quote { get; set; }
+        public string Summary { get; set; }
+        public string Commentary { get; set; }
+        public List<string> Keywords = new List<string>();
+    }
+}
